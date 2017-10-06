f01bdb4529be30902f0a31d41f73a28aeb0e6630
ekk-amboss <ekk@miamed.de>
2017-10-06 05:36:55 +0200

    Update impress.js

--- js/impress.js
+++ js/impress.js
@@ -703,13 +703,13 @@
         // KEYBOARD NAVIGATION HANDLERS
 
         // Prevent default keydown action when one of supported key is pressed.
-      //  document.addEventListener( "keydown", function( event ) {
-      //      if ( event.keyCode === 9 ||
-      //         ( event.keyCode >= 32 && event.keyCode <= 34 ) ||
-      //         ( event.keyCode >= 37 && event.keyCode <= 40 ) ) {
-      //          event.preventDefault();
-      //      }
-     //   }, false );
+       document.addEventListener( "keydown", function( event ) {
+            if ( event.keyCode === 9 ||
+              ( event.keyCode >= 32 && event.keyCode <= 34 ) ||
+             ( event.keyCode >= 37 && event.keyCode <= 40 ) ) {
+              event.preventDefault();
+            }
+          }, false );
 
         // Trigger impress action (next or prev) on keyup.
 
@@ -733,18 +733,20 @@
             }
 
             if ( event.keyCode === 9 ||
-               ( event.keyCode >= 32 && event.keyCode <= 34 ) ||
-               ( event.keyCode >= 37 && event.keyCode <= 40 ) ) {
+                event.keyCode === 33 ||
+                event.keyCode === 34 ||
+                event.keyCode === 38 ||
+                event.keyCode === 40  ) {
                 switch ( event.keyCode ) {
                     case 33: // Page up
-                    case 37: // Left
+                  //  case 37: // Left
                     case 38: // Up
                              api.prev();
                              break;
                     case 9:  // Tab
-                    case 32: // Space
+                 //   case 32: // Space
                     case 34: // Page down
-                    case 39: // Right
+                  //  case 39: // Right
                     case 40: // Down
                              api.next();
                              break;
